3ed2d4228ef8ce9b64968ddc2ce816e63a78d8b3
Ferdinan Kurnia <ferdinankurnian@gmail.com>
2025-10-25 00:44:13 +0700

    fixing cloudflare setup

--- src/steps/setup-cloudflare.ts
+++ src/steps/setup-cloudflare.ts
@@ -1,34 +1,37 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 import { addDeps } from './add-deps';
-import { Ctx } from '../types';
+import chalk from 'chalk';
 
-export const setupCloudflare = async (ctx: Ctx) => {
-  console.log('Setting up Cloudflare...');
+export const setupCloudflare = async ({ projectDir, projectName }: { projectDir: string, projectName: string }) => {
+  console.log();
+  console.log(chalk.blue('[◉] Setting up Cloudflare...'));
 
-  // TODO:
   // 1. Add wrangler as a dev dependency
-  await addDeps(ctx, ['wrangler'], { dev: true });
+  const pkgJsonPath = path.join(projectDir, "package.json");
+  const pkgJson = JSON.parse(await fs.readFile(pkgJsonPath, "utf-8"));
+  const newPkg = await addDeps(pkgJson, [{ name: 'wrangler', isDev: true }], []);
+  await fs.writeFile(pkgJsonPath, JSON.stringify(newPkg, null, 2));
+
   // 2. Create wrangler.toml file
   const wranglerTomlContent = `
-name = "${ctx.projectName}"
+name = "${projectName}"
 compatibility_date = "${new Date().toISOString().split('T')[0]}"
 
 [pages]
 build_output_dir = "dist"
 `;
   await fs.writeFile(
-    path.join(ctx.projectDir, "wrangler.toml"),
+    path.join(projectDir, "wrangler.toml"),
     wranglerTomlContent.trim()
   );
   // 3. Add "deploy" script to package.json
-  const pkgJsonPath = path.join(ctx.projectDir, "package.json");
-  const pkgJson = JSON.parse(await fs.readFile(pkgJsonPath, "utf-8"));
-  pkgJson.scripts = {
-    ...pkgJson.scripts,
+  const finalPkgJson = JSON.parse(await fs.readFile(pkgJsonPath, "utf-8"));
+  finalPkgJson.scripts = {
+    ...finalPkgJson.scripts,
     deploy: "bun run build && wrangler pages deploy",
   };
-  await fs.writeFile(pkgJsonPath, JSON.stringify(pkgJson, null, 2));
+  await fs.writeFile(pkgJsonPath, JSON.stringify(finalPkgJson, null, 2));
 
-  console.log('Cloudflare setup complete!');
+  console.log(chalk.blue('[◉] Cloudflare setup complete!'));
 };
